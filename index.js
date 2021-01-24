ef3afeba2afbdd1fe627d2d6a4201649b6a48850
S M AL AMIN <76505639+alamin93@users.noreply.github.com>
2021-01-24 11:07:45 +0600

    Add files via upload
    addevenlister and annonimus function use

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var myhead = document.querySelector("h1");
+myhead.addEventListener("mouseover",function (){
+    myhead.classList.add("head1")
+
+});
+
+myhead.addEventListener("mouseout",function (){
+    myhead.classList.remove("head1")
+
+});
